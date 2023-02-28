e876a0c4b293d0f6f5749b7efbd046129db710f6
Nicholas <crazedglitch@yahoo.com>
2023-02-27 18:05:25 -0600

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const stringLine = document.querySelector("p")
+    stringLine.textContent = "This is really cool!" 
+});
+
